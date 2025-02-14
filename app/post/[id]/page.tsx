bc41439434815d41356c179f6bee370ceac0b314
rmymartinet <remymartinet.contact@gmail.com>
2025-02-14 14:02:17 +0100

    Refactor PostId component to improve type definitions and fetch logic

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,12 +1,15 @@
 import BackButton from "@/app/components/Backbutton";
 import PostDetails from "@/app/components/Blog/PostDetails";
 import React from "react";
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
 
-export default async function PostId({ params }: { params: { id: string } }) {
+export default async function PostId({ params }: PageProps) {
   const { id } = await params;
 
-  const post = fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  const postJson = await (await post).json();
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const postJson = await res.json();
 
   return (
     <div className="mt-40 flex flex-col gap-20">
